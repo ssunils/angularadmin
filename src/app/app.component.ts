b3e5f63d81a62d2643241bac60b2f650fe22473f
Sunil <ssunils2004@gmail.com>
2017-08-14 17:38:23 +0800

    GA and remove unwanted files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router, ActivatedRoute, NavigationEnd, Event } from '@angular/router';
 declare var jQuery:any;
+declare var ga:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,10 +14,10 @@ export class AppComponent {
   constructor(private titleService: Title, router: Router, activatedRoute: ActivatedRoute) {
     router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        console.log(this.getTitle(router.routerState, router.routerState.root))
         var title = this.getTitle(router.routerState, router.routerState.root).join(' | ');
-        console.log('title', title);
         titleService.setTitle(title);
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
       }
     });
   }
