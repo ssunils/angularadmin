d96bfba3a093764c3982252c8e626832d0421a2d
Sunil <ssunils2004@gmail.com>
2017-10-27 16:19:45 +0800

    Test CD

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 // Run the app by serving the static files
-// in the dist directory
+// in the dist directory //
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
